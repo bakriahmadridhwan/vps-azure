21a987341ded6c40ad409f5084f16ad6c645f70f
bakriahmadridhwan <bahridhwan@gmail.com>
2024-02-07 15:19:34 +0700

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = "Bakri";
+$nim = 1576;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan VPS Azure</title>
+</head>
+
+<body>
+
+    <h1><?= $name ?></h1>
+    <p><?= $nim ?></p>
+
+</body>
+
+</html>
